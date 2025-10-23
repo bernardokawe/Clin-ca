44080c42e9ed805fc82d844405f96062a10ccfa0
bernardokawe <bernardokawe9@gmail.com>
2025-10-23 15:49:36 -0300

    commitinicia

--- create.php
+++ create.php
@@ -7,16 +7,16 @@
     <form id="create-form" action="<?= $BASE_URL ?>config/process.php" method="POST">
       <input type="hidden" name="type" value="create">
       <div class="form-group">
-        <label for="name">Nome do contato:</label>
-        <input type="text" class="form-control" id="name" name="name" placeholder="Digite o nome" required>
+        <label for="name">Raça do cachorro:</label>
+        <input type="text" class="form-control" id="name" name="name" placeholder="Digite a raça" required>
       </div>
       <div class="form-group">
-        <label for="phone">Telefone do contato:</label>
-        <input type="text" class="form-control" id="phone" name="phone" placeholder="Digite o telefone" required>
+        <label for="phone">Telefone de contato do dono:</label>
+        <input type="text" class="form-control" id="phone" name="phone" placeholder="Digite o telefone do dono" required>
       </div>
       <div class="form-group">
         <label for="observations">Observações:</label>
-        <textarea type="text" class="form-control" id="observations" name="observations" placeholder="Insira as observações" rows="3"></textarea>
+        <textarea type="text" class="form-control" id="observations" name="observations" placeholder="Qual trabalho devemos realizar?" rows="3"></textarea>
       </div>
       <button type="submit" class="btn btn-primary">Cadastrar</button>
     </form>
